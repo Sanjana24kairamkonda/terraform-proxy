f726b4b981864a7f4709333fc99bf5fdb10ca22a
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2025-03-11 11:04:26 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP Zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "vm_name" {
+  description = "Cloud SQL Proxy VM"
+  type        = string
+  default     = "cloudsql-proxy-vm"
+}
+
+variable "db_instance_connection_name" {
+  description = "Cloud SQL instance connection name (format: project:region:instance)"
+  type        = string
+}
